eb265f89fb04db402646155a2fe460d800a9d0e2
Roy Gero <roy.gero.ii@gmail.com>
2025-08-10 20:08:15 -0400

    Migrating the Key Listener prototype from the Death Counter Project

---
+++ Key Listener/src/utilities/index.js
@@ -0,0 +1,22 @@
+const { loadConfig, validateConfig, watchConfig, saveConfig } = require('./configUtils');
+const { createSocket, safeEmit } = require('./socketUtils');
+const { modifiersMatch, keyBindingMatches, getKeyBindingString, validateKeyBinding, findMatchingBindings} = require('./keyboardUtils');
+
+module.exports = {
+  // Config utilities
+  loadConfig,
+  validateConfig,
+  watchConfig,
+  saveConfig,
+
+  // Socket utilities
+  createSocket,
+  safeEmit,
+
+  // Keyboard utilities
+  modifiersMatch,
+  keyBindingMatches,
+  getKeyBindingString,
+  validateKeyBinding,
+  findMatchingBindings
+};
